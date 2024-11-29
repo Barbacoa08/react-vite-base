678110deec29adcbc005e06009a9c0bac9806592
barbajoe <sabe12070@gmail.com>
2024-11-29 16:06:13 -0600

    add all the basics (#2)
    * cleanup
    
    * add testing
    
    * fix paths
    
    * add barbajoe css
    
    * code rabbit suggestions

---
+++ src/components/App/App.test.tsx
@@ -0,0 +1,18 @@
+import { axe } from "jest-axe";
+import { render, screen } from "test-utils";
+import { App } from "./";
+
+describe("App component", () => {
+	it("opens without exploding", () => {
+		render(<App />);
+
+		expect(screen.getByRole("main")).toBeDefined();
+	});
+
+	it("passes basic axe compliance", async () => {
+		const { container } = render(<App />);
+		const results = await axe(container);
+
+		expect(results).toHaveNoViolations();
+	});
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import "@barbajoe/css-lib/dist/barbajoe.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+
+import { App } from "components";
 
 // biome-ignore lint/style/noNonNullAssertion: trust vite on this one
 createRoot(document.getElementById("root")!).render(
