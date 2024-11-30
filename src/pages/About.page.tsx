2af03ff89d2db69de917dd7b32535e9179892c41
barbajoe <sabe12070@gmail.com>
2024-11-30 15:08:16 -0600

    add global context for fun-sies

--- src/pages/About.page.tsx
+++ src/pages/About.page.tsx
@@ -1,8 +1,47 @@
+import { useCallback } from "react";
+import { useGlobalContext } from "src/GlobalContext";
+
+const nameInputId = "name-input";
+
 export const AboutPage = () => {
+	const { name, setName } = useGlobalContext();
+
+	const handleFormSubmit = useCallback(
+		// biome-ignore lint/suspicious/noExplicitAny: form types, am I right?
+		(e: any) => {
+			const updatedName = e.target[nameInputId].value.trim();
+			e.preventDefault();
+			e.target.reset();
+
+			if (updatedName) {
+				setName(updatedName);
+			}
+		},
+		[setName],
+	);
+
 	return (
 		<main>
 			<h1>About</h1>
-			<p>This is the about page.</p>
+
+			<p>
+				This is the about page. You can change your name here. Your current
+				defined name is: {name}
+			</p>
+
+			<form onSubmit={handleFormSubmit}>
+				<label>
+					New Name
+					<input
+						id={nameInputId}
+						name={nameInputId}
+						type="text"
+						maxLength={25}
+					/>
+				</label>
+
+				<button type="submit">Submit</button>
+			</form>
 		</main>
 	);
 };
