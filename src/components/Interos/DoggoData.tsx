c8a75a4e3f77a247e8901918318468dcdead5910
barbajoe <sabe12070@gmail.com>
2025-02-01 16:29:30 -0600

    funsies

--- src/components/Interos/DoggoData.tsx
+++ src/components/Interos/DoggoData.tsx
@@ -1,4 +1,6 @@
-import { use, useEffect, useState } from "react";
+import { use, useEffect, useId, useMemo, useState } from "react";
+
+const stringSeperator = ", ";
 
 interface ApiResult {
 	bred_for?: string;
@@ -24,8 +26,13 @@ interface ApiImage {
 	url: string;
 }
 
+interface DoggoForm {
+	temperament?: string;
+	purpose?: string;
+}
+
 const dogApiRoot = "https://api.thedogapi.com/";
-const dogBreeds = (limit = 10, page = 0) =>
+const dogBreeds = (limit = 100, page = 0) =>
 	`v1/breeds?limit=${limit}&page=${page}`;
 
 const fetchDogData = async (): Promise<ApiResult[]> => {
@@ -48,7 +55,9 @@ export const DoggoData = () => {
 	const data = use(getDogData);
 
 	const [temperaments, setTemperaments] = useState<string[]>([]);
-	const [purpose, setPurpose] = useState<string[]>([]);
+	const [purposes, setPurposes] = useState<string[]>([]);
+
+	const [formData, setFormData] = useState<DoggoForm>({});
 
 	useEffect(() => {
 		const tOptions = new Set<string>();
@@ -57,13 +66,13 @@ export const DoggoData = () => {
 		for (let i = 0; i < data.length; i++) {
 			const breed = data[i];
 
-			const t = breed.temperament.split(", ");
+			const t = breed.temperament.split(stringSeperator);
 			for (let j = 0; j < t.length; j++) {
 				const breedTemperment = t[j];
 				tOptions.add(breedTemperment);
 			}
 
-			const p = breed.bred_for?.split(", ") ?? [];
+			const p = breed.bred_for?.split(stringSeperator) ?? [];
 			for (let j = 0; j < p.length; j++) {
 				const breedPurpose = p[j];
 				pOptions.add(breedPurpose);
@@ -71,31 +80,98 @@ export const DoggoData = () => {
 		}
 
 		setTemperaments(Array.from(tOptions).toSorted());
-		setPurpose(Array.from(pOptions).toSorted());
+		setPurposes(Array.from(pOptions).toSorted());
 	}, [data]);
 
 	return (
 		<section>
 			<h2>Your Best Doggo</h2>
 
-			<DoggoSelect options={temperaments} />
-			<DoggoSelect options={purpose} />
+			<DoggoSelect
+				label="Temperament"
+				options={temperaments}
+				onSelect={(v) => setFormData((prev) => ({ ...prev, temperament: v }))}
+			/>
+			<DoggoSelect
+				label="Bred For Purpose"
+				options={purposes}
+				onSelect={(v) => setFormData((prev) => ({ ...prev, purpose: v }))}
+			/>
+
+			<BestDoggo doggos={data} formData={formData} />
 		</section>
 	);
 };
 
-const DoggoSelect = ({ options = [] }: { options: string[] }) => {
+const DoggoSelect = ({
+	label,
+	options = [],
+	onSelect,
+	id = useId(),
+}: {
+	label: string;
+	options: string[];
+	onSelect: (v: string) => void;
+	id?: string;
+}) => {
 	if (options.length <= 0) return <div>loading...</div>;
 
 	return (
-		<select>
-			<option>NONE SELECTED</option>
+		<div>
+			<label htmlFor={id}>{label}</label>
 
-			{options.map((o) => (
-				<option key={o} value={o}>
-					{o}
-				</option>
-			))}
-		</select>
+			<select id={id} onChange={(e) => onSelect(e.target.value)}>
+				<option value="">NONE SELECTED</option>
+
+				{options.map((o) => (
+					<option key={o} value={o}>
+						{o}
+					</option>
+				))}
+			</select>
+		</div>
+	);
+};
+
+const BestDoggo = ({
+	doggos,
+	formData: { purpose, temperament },
+}: { doggos: ApiResult[]; formData: DoggoForm }) => {
+	const matches = useMemo(() => {
+		return purpose && temperament
+			? doggos
+					.filter(
+						(d) =>
+							d.bred_for?.includes(purpose) &&
+							d.temperament.split(stringSeperator).includes(temperament),
+					)
+					.map((d) => d.name)
+			: [];
+	}, [doggos, purpose, temperament]);
+
+	return (
+		<section>
+			<h3>Results</h3>
+
+			<section>
+				{purpose || temperament ? (
+					<>
+						<p>Temperament: {temperament || "not yet selected"}</p>
+						<p>Purpose: {purpose || "not yet selected"}</p>
+					</>
+				) : (
+					<p>No selectios made</p>
+				)}
+			</section>
+
+			<section>
+				<p>
+					True Match result:{" "}
+					{matches.length
+						? matches.join(stringSeperator)
+						: "no matches found 😭"}
+				</p>
+			</section>
+		</section>
 	);
 };
