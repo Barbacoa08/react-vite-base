f48d1414edb6f368ca315d647afa3511aa659b85
barbajoe <sabe12070@gmail.com>
2025-02-01 14:50:34 -0600

    notes

--- src/components/Interos/WeatherChart.tsx
+++ src/components/Interos/WeatherChart.tsx
@@ -1,26 +1,52 @@
-import { useMemo } from "react";
+import { use } from "react";
 
-const call = `https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_WEATHER_API}&q=55104&aqi=no`;
+const dogApiRoot = "https://api.thedogapi.com/";
+const dogBreeds = (limit = 100, page = 0) =>
+	`v1/breeds?limit=${limit}&page=${page}`;
+
+const fetchDogData = async () => {
+	const call = `${dogApiRoot}${dogBreeds()}`;
+	const response = await fetch(call, {
+		headers: {
+			"x-api-key": import.meta.env.VITE_DOG_API,
+		},
+	});
+
+	if (!response.ok) {
+		throw new Error("Failed to fetch weather");
+	}
+
+	return response.json();
+};
+const getDogData = fetchDogData();
+
+// 86400000: (24 * 60 * 60 * 1000) + {days}
+// const todayPlusDay = (days: number) => {
+// 	const date = new Date(Date.now() - (86400000 + days));
+// 	return date.toISOString();
+// };
+// const weatherApiBase = "https://api.weatherapi.com/v1/current.json";
+// const fetchWeather = async (postalcode = "55104", pastDays = 7) => {
+// 	const call = `${weatherApiBase}?key=${import.meta.env.VITE_WEATHER_API}&q=${postalcode}&aqi=no&dt=${todayPlusDay(-pastDays)}`;
+// 	const response = await fetch(call);
+// 	if (!response.ok) {
+// 		throw new Error("Failed to fetch weather");
+// 	}
+// 	return response.json();
+// };
+// const getWeatherData = fetchWeather();
 
 export const WeatherChart = () => {
-	const data = useMemo(async () => {
-		const response = await fetch(call);
-
-		if (!response.ok) {
-			console.error(`${response.status} - ${response.statusText}`);
-		} else {
-			const result = await response.json();
-			console.log("result", result);
-			return result;
-		}
-	}, []);
+	const data = use(getDogData);
 	console.log("data", data);
 
 	return (
 		<section>
 			<h2>Weather Data</h2>
 
-			<p>{JSON.stringify(data)}</p>
+			{/* <p>
+				{data.location.name}, {data.location.region}
+			</p> */}
 		</section>
 	);
 };
